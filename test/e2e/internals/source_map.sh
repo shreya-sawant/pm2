a325004d41436fbfe91e0e06f50ff343a51aa871
Shreya Sawant <90320606+shreya-sawant@users.noreply.github.com>
2025-04-30 13:27:30 +0530

    Update source_map.sh

--- test/e2e/internals/source_map.sh
+++ test/e2e/internals/source_map.sh
@@ -10,21 +10,21 @@ cd $file_path
 #
 rm sm.log
 $pm2 start source-map/main.js -e sm.log --merge-logs --disable-source-map-support
-sleep 2
+sleep 4
 cat sm.log | grep "main.js"
 spec "should not take source map into account"
 
 rm sm.log
 $pm2 delete all
 $pm2 start source-map/main.js -e sm.log --merge-logs
-sleep 2
+sleep 4
 cat sm.log | grep "main.ts"
 spec "should automatically activate source map support (detect main.ts)"
 
 rm sm.log
 $pm2 delete all
 $pm2 start source-map/main.js -e sm.log --merge-logs --source-map-support
-sleep 2
+sleep 4
 cat sm.log | grep "main.ts"
 spec "should force source map support"
 
@@ -34,13 +34,13 @@ spec "should force source map support"
 rm sm.log
 $pm2 delete all
 $pm2 start source-map/main.js -e sm.log --merge-logs -i 1
-sleep 2
+sleep 4
 cat sm.log | grep "main.ts"
 spec "should automatically activate source map support (detect main.ts)"
 
 rm sm.log
 $pm2 delete all
 $pm2 start source-map/main.js -e sm.log --merge-logs -i 1 --source-map-support
-sleep 2
+sleep 4
 cat sm.log | grep "main.ts"
 spec "should force source map support"
